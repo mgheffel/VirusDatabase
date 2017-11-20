2b9250d4c4ddee5492e6882f9d2dc04adaf743bd
levnikolaj <levnikolaj@gmail.com>
2017-11-20 16:57:10 -0600

    MySql.Data.dll installed and referenced

--- VirusDataApplication/VirusDataApplication/DatabaseModel.cs
+++ VirusDataApplication/VirusDataApplication/DatabaseModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace VirusDataApplication{
     /// <summary>
@@ -10,5 +11,7 @@ namespace VirusDataApplication{
     /// </summary>
     static class DatabaseModel{
         
+
+
     }
 }
