2ab17c4279cc65fb2d09a7f15d3b754aa1c3f490
levnikolaj <levnikolaj@gmail.com>
2017-11-20 15:48:33 -0600

    Made DatabaseModel Static

--- VirusDataApplication/VirusDataApplication/DatabaseModel.cs
+++ VirusDataApplication/VirusDataApplication/DatabaseModel.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace VirusDataApplication
-{
-    class DatabaseModel
-    {
+namespace VirusDataApplication{
+    /// <summary>
+    /// Making the database model class static, because there will be only one. The controller will then call methods from it directly.
+    /// </summary>
+    static class DatabaseModel{
+
+
+
+
     }
 }
