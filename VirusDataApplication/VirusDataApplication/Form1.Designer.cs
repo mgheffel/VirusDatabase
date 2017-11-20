66f4deeddd07ee3c93ab204cfc0a65e07e29bd80
smoylan22 <smoylan22@gmail.com>
2017-11-20 15:48:43 -0600

    added controller object and a temporary gui

--- VirusDataApplication/VirusDataApplication/Form1.Designer.cs
+++ VirusDataApplication/VirusDataApplication/Form1.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.input_tb = new System.Windows.Forms.TextBox();
+            this.send_btn = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // input_tb
+            // 
+            this.input_tb.Location = new System.Drawing.Point(23, 12);
+            this.input_tb.Name = "input_tb";
+            this.input_tb.Size = new System.Drawing.Size(222, 26);
+            this.input_tb.TabIndex = 0;
+            // 
+            // send_btn
+            // 
+            this.send_btn.Location = new System.Drawing.Point(251, 15);
+            this.send_btn.Name = "send_btn";
+            this.send_btn.Size = new System.Drawing.Size(75, 32);
+            this.send_btn.TabIndex = 1;
+            this.send_btn.Text = "submit";
+            this.send_btn.UseVisualStyleBackColor = true;
+            this.send_btn.Click += new System.EventHandler(this.send_btn_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(23, 53);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(303, 458);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 560);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.send_btn);
+            this.Controls.Add(this.input_tb);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox input_tb;
+        private System.Windows.Forms.Button send_btn;
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
