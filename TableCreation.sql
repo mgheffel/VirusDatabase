2851b7b16941b37df986f5247292a7f5a0790018
mgheffel <mgheffel@gmail.com>
2017-11-20 15:48:01 -0600

    data

--- TableCreation.sql
+++ TableCreation.sql
@@ -4,7 +4,6 @@ DROP TABLE IF EXISTS Publication_Researcher;
 DROP TABLE IF EXISTS Researchers;
 DROP TABLE IF EXISTS Publications;
 DROP TABLE IF EXISTS Strain_Protein;
-DROP TABLE IF EXISTS Strain_ORF;
 DROP TABLE IF EXISTS OpenReadingFrames;
 DROP TABLE IF EXISTS Strains;
 DROP TABLE IF EXISTS Species;
@@ -15,7 +14,7 @@ DROP TABLE IF EXISTS Proteins;
 
 create table Proteins(
 	pID int primary key,
-	pName varchar(50) not null
+	pType varchar(15) not null
 );
 create table StructuralProteins(
 	pID int primary key,
@@ -44,26 +43,19 @@ create table Strains(
 );
 create table OpenReadingFrames(
 	strainID varchar(10) not null,
-	orfNumber varchar(10) not null,
+	orfID varchar(10) not null,
 	pID int,
 	startIndex int not null,
 	stopIndex int not null,
 	foreign key (pID) references Proteins(pID),
-	PRIMARY KEY (strainID, orfNumber)
+	PRIMARY KEY (strainID, orfID)
 );
-
 create table Strain_Protein(
 	strainID varchar(10),
 	pID int,
 	foreign key (strainID) references Strains(strainID),
-	foreign key (pID) references Proteins(pID)
-);
-
-create table Strain_ORF(
-	strainID varchar(10),
-	seqID int,
-	foreign key (strainID) references Strains(strainID),
-	foreign key (seqID) references OpenReadingFrame(seqID)
+	foreign key (pID) references Proteins(pID),
+	primary key (strainID, pID)
 );
 
 create table Publications(
@@ -82,16 +74,18 @@ create table Publication_Researcher(
 	rID int,
 	pubID int,
 	foreign key (rID) references Researchers(rID),
-	foreign key (pubID) references Publications(pubID)
+	foreign key (pubID) references Publications(pubID),
+	primary key  (pubID, rID)
 );
 create table Publishers(
 	publisherID int primary key,
-	name varchar(50) not null
+	name varchar(100) not null
 );
 
-create table Pubublisher_Publication(
+create table Publisher_Publication(
 	publisherID int,
 	pubID int,
 	foreign key (publisherID) references Publishers(publisherID),
-	foreign key (pubID) references Publications(pubID)
+	foreign key (pubID) references Publications(pubID),
+	primary key (publisherID, pubID)
 )
